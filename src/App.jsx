5489c99bd6f8fbef4ae6a84aa6472ca086dc2111
nailton-vieira <nailtonvieira@gmail.com>
2025-06-19 22:55:03 -0300

    Enviando update de app.jsx ao repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import HeaderMobile from "./components/HeaderMobile";
+
+
 function App () {
   return (
     <div className="min-h-screen flex flex-col justify-between bg-white" >
+       <HeaderMobile />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
 
     </div>
     );
